dad83177ebf2eda0fdfe242daf8657fb0e1ef087
Samishan11 <samishanthapa0@gmail.com>
2023-03-27 11:44:27 +0545

    login user

--- src/controller/user.contorller/user.controller.js
+++ src/controller/user.contorller/user.controller.js
@@ -1,7 +1,11 @@
 const userModel = require("../../models/user.model");
 var { StatusCodes } = require('http-status-codes');
+const bcryptjs = require('bcryptjs');
+const auth = require("../../middleware/auth")
+const jwt = require("jsonwebtoken")
+var adminAttemptCount = 0, blockEmail;
 
-const bcryptjs = require('bcryptjs')
+// 
 exports.registerUser = async (req, res) => {
     try {
         const { username, email, password, checkpassword, phone } = req.body;
@@ -39,11 +43,65 @@ exports.registerUser = async (req, res) => {
                 return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('500 SERVER ERROR!!');
             })
         })
-
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
             success: false,
             message: "Something went wrong!!"
+        })
+    }
+}
+
+exports.loginUser = async(req,res) => {
+    const { email, password } = req.body;
+
+    if (typeof email !== "string" || typeof password !== "string") {
+        return res.status(StatusCodes.UNAUTHORIZED).send("Client side validation issues. Please carefully send the right format of email and password !!")
+    }
+
+    if (adminAttemptCount > 4 && blockEmail === email) {
+        return res.status(StatusCodes.FORBIDDEN).send({
+            message: 'You exceed the 5 login attempt. Please try again after 5 min !!',
+        });
+    }
+
+    try {
+        const data = await userModel.findOne({ email: email })
+
+        if (data !== undefined) {
+            const isMatched = await data?.matchPassword(password);
+
+            if (!isMatched) {
+                return res.status(StatusCodes.UNAUTHORIZED).send({
+                    success: false,
+                    message: "Email or Password didn't matched"
+                })
+            }
+
+            const { accessToken, refreshToken } = auth.GenerateJWT({data});
+            const token = jwt.sign({ _id: data._id, username: data.username, email: data.email, isAdmin: data.isAdmin, verified: data.verified },  process.env.ACCESS_TOKEN_KEY)
+            return res.status(200).send({
+                message: 'Login succesfully.',
+                token: token,
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+                isAdmin: data.isAdmin,
+                isVerified: data.isVerifed
+            })
+        } else {
+            adminAttemptCount++;
+            if (adminAttemptCount === 5) {
+                blockEmail = email;
+                setTimeout(() => {
+                    adminAttemptCount = 0;
+                    blockEmail = null;
+                    console.log(`${email} you can login. => ${adminAttemptCount}`);
+                }, 300000);
+            }
+            return res.status(StatusCodes.UNAUTHORIZED).send('Wrong email or password !!');
+        }
+    } catch (err) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+            message: err.message
         })
     }
 }
--- src/routes/user.route/user.route.js
+++ src/routes/user.route/user.route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {registerUser} = require('../../controller/index.controller').user
+const {registerUser , loginUser} = require('../../controller/index.controller').user
 // routes
 router.post('/register-user', registerUser);
+router.post('/login-user', loginUser);
+
 module.exports = router;
