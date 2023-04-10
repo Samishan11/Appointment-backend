83570f64186f20ad80606fa2ca86e5bd2708a481
Samishan11 <samishanthapa0@gmail.com>
2023-04-10 15:47:35 +0545

    changes

--- src/controller/booking.controller/booking.controller.js
+++ src/controller/booking.controller/booking.controller.js
@@ -1,85 +1,84 @@
-const booking = require('../../models/booking.model');
+const booking = require("../../models/booking.model");
 const { StatusCodes } = require("http-status-codes");
-const appointmentModel = require('../../models/appointment.model')
-// appointment booking 
+const appointmentModel = require("../../models/appointment.model");
+// appointment booking
 exports.booking = async (req, res) => {
-    try {
-        const { appointment, username, email, booked_on } = req.body;
-        const _appointment = await appointmentModel.findOne({ _id: appointment })
-        console.log(req.body)
-        console.log(req.body)
-        const _booking = await new booking({
-            appointment,
-            booked_on: new Date(),
-            username,
-            email
-        })
-        if (_booking) {
-            _appointment.isBooked = true;
-        }
-        await _booking.save()
-        await _appointment.save()
-        return res.status(StatusCodes.OK).send({
-            success: true,
-            message: "Appointment has been booked",
-            data: _booking
-        })
-    } catch (error) {
-        console.log(error)
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            success: false,
-            message: "INTERNAL_SERVER_ERROR !!"
-        })
-    }
-}
+  try {
+    const { appointment, username, email, booked_on } = req.body;
+    const _appointment = await appointmentModel.findOne({ _id: appointment });
+    console.log(_appointment);
+    console.log(req.body);
+    console.log(req.body);
+    const _booking = await new booking({
+      appointment,
+      booked_on: new Date(),
+      username,
+      email,
+    });
+
+    await _booking.save();
+    return res.status(StatusCodes.OK).send({
+      success: true,
+      message: "Appointment has been booked",
+      data: _booking,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      success: false,
+      message: "INTERNAL_SERVER_ERROR !!",
+    });
+  }
+};
 
 // appointment booking update
 exports.bookingUpdate = async (req, res) => {
-    try {
-        const { appointment, booked_on } = req.body;
-        var _update = await booking.findOneAndUpdate({ _id: req.params.id }, req?.body);
-        return res.status(StatusCodes.OK).send({
-            success: true,
-            message: "Appointment has been updated"
-        })
-
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            success: false,
-            message: "INTERNAL_SERVER_ERROR !!"
-        })
-    }
-}
+  try {
+    const { appointment, booked_on } = req.body;
+    var _update = await booking.findOneAndUpdate(
+      { _id: req.params.id },
+      req?.body
+    );
+    return res.status(StatusCodes.OK).send({
+      success: true,
+      message: "Appointment has been updated",
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      success: false,
+      message: "INTERNAL_SERVER_ERROR !!",
+    });
+  }
+};
 
 // appointment booking delete
 exports.bookingDelete = async (req, res) => {
-    try {
-        var _delete = await booking.findOneAndDelete({ _id: req.params.id });
-        return res.status(StatusCodes.OK).send({
-            success: true,
-            message: "Appointment has been deleted"
-        })
-
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            success: false,
-            message: "INTERNAL_SERVER_ERROR !!"
-        })
-    }
-}
+  try {
+    var _delete = await booking.findOneAndDelete({ _id: req.params.id });
+    return res.status(StatusCodes.OK).send({
+      success: true,
+      message: "Appointment has been deleted",
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      success: false,
+      message: "INTERNAL_SERVER_ERROR !!",
+    });
+  }
+};
 
 // appointment booking get
 exports.bookingGet = async (req, res) => {
-    try {
-        var _get = await booking.find();
-        return res.status(StatusCodes.OK).send({
-            success: true,
-            data: _get
-        })
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            success: false,
-            message: "INTERNAL_SERVER_ERROR !!"
-        })
-    }
-}
+  try {
+    var _get = await booking.find();
+    return res.status(StatusCodes.OK).send({
+      success: true,
+      data: _get,
+    });
+  } catch (error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      success: false,
+      message: "INTERNAL_SERVER_ERROR !!",
+    });
+  }
+};
