4f0e105f90570b727c132830aa635ebe92ec0a1c
Samishan11 <samishanthapa0@gmail.com>
2023-04-17 11:00:59 +0545

    error fix

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const DB_URI = process.env.DB_URI
+const mongoose = require("mongoose");
+require("dotenv").config();
+const DB_URI = process.env.DB_URI;
 
-// database connection 
-const connectDB = async = () => {
-    try {
-        mongoose.connect(DB_URI).then(()=> {
-            console.log('Database connected')
-        })
-    } catch (error) {
-        // error 
-        console.log(error.message)
-    }
-}
+// database connection
+const connectDB = (async = () => {
+  try {
+    mongoose.connect(DB_URI).then(() => {
+      console.log("Database connected");
+    });
+  } catch (error) {
+    // error
+    console.log(error.message);
+  }
+});
 
 module.exports = connectDB;
